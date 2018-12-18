dc60c6d1ebe0142dba4b3f0290681642de86aebb
Elnatan Steinmetz <elnatan@ravtech.co.il>
2018-12-18 12:19:23 +0200

    add moer features to wait

--- appium_selenium_driver/wait_for_elements_tools.py
+++ appium_selenium_driver/wait_for_elements_tools.py
@@ -9,9 +9,10 @@ class WaitForElementTools:
     def __init__(self, driver):
         self.driver = driver
 
-    def wait_for_element_to_be_present(self, selector, driver=None, timeout=30):
+    def wait_for_element_to_be_present(self, selector, driver=None, timeout=30, raise_exception=True):
         driver = driver or self.driver
         try:
+            ElementsTools.take_screenshot(driver=self.driver)
             element = WebDriverWait(driver=driver, timeout=timeout).until(ec.presence_of_element_located(selector))
             if not element:
                 raise NoSuchElementException
@@ -20,11 +21,27 @@ class WaitForElementTools:
         except Exception as e:
             ElementsTools.take_screenshot(driver=self.driver, filename='element_not_found')
             print("element {} not found exception: {}".format(selector, e))
-            raise e
+            if raise_exception:
+                raise e
+            return False
 
-    def wait_for_element_to_be_clickable(self, selector, driver=None, timeout=30):
+    def wait_for_element_to_be_invisible(self, selector, driver=None, timeout=30):
         driver = driver or self.driver
         try:
+            ElementsTools.take_screenshot(driver=self.driver)
+            element = WebDriverWait(driver=driver, timeout=timeout).until(ec.invisibility_of_element(selector))
+        except Exception as e:
+            ElementsTools.take_screenshot(driver=self.driver, filename='element_not_found')
+            print("element {} found exception: {}".format(selector, e))
+            raise e
+        if not element:
+            raise Exception("element still visible")
+        return True
+
+    def wait_for_element_to_be_clickable(self, selector, driver=None, timeout=30, raise_exception=True):
+        driver = driver or self.driver
+        try:
+            ElementsTools.take_screenshot(driver=self.driver)
             element = WebDriverWait(driver=driver, timeout=timeout).until(ec.element_to_be_clickable(selector))
             if not element:
                 raise NoSuchElementException
@@ -33,11 +50,14 @@ class WaitForElementTools:
         except Exception as e:
             ElementsTools.take_screenshot(driver=self.driver, filename='element_not_found')
             print("element {} not found exception: {}".format(selector, e))
-            raise e
+            if raise_exception:
+                raise e
+            return False
 
-    def wait_for_elements_to_be_present(self, selector, driver=None, timeout=30):
+    def wait_for_elements_to_be_present(self, selector, driver=None, timeout=30, raise_exception=True):
         driver = driver or self.driver
         try:
+            ElementsTools.take_screenshot(driver=self.driver)
             elements = WebDriverWait(driver=driver, timeout=timeout). \
                 until(ec.visibility_of_all_elements_located(selector))
             if not elements:
@@ -47,12 +67,15 @@ class WaitForElementTools:
         except Exception as e:
             ElementsTools.take_screenshot(driver=self.driver, filename='element_not_found')
             print("element {} not found exception: {}".format(selector, e))
-            raise e
+            if raise_exception:
+                raise e
+            return False
 
-    def wait_for_element_contain_text(self, selector, driver=None, text_contain=None, timeout=30):
+    def wait_for_element_contain_text(self, selector, driver=None, text_contain=None, timeout=30, raise_exception=True):
         by, selector_new = selector
         new_selector = (by, selector_new.format(text=text_contain))
-        self.wait_for_elements_to_be_present(selector=new_selector, driver=driver, timeout=timeout)
+        self.wait_for_elements_to_be_present(selector=new_selector, driver=driver, timeout=timeout,
+                                             raise_exception=raise_exception)
 
     def wait_for_element_to_be_invisible(self, selector, driver=None, timeout=30):
         driver = driver or self.driver
